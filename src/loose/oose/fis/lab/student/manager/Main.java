4ef57ddd1a9ac734d5c8aeee3acddfcd6a929684
unknown <gealapu_andreea@yahoo.com>
2019-04-07 14:16:58 +0300

    SM-2 added th students table and some dummy student list

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        primaryStage.setTitle("student manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
